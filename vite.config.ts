fce490b2521025e9f777b6ade78fa3a4ef17017a
kostili-tec <weh4teyou@gmail.com>
2023-03-10 15:38:37 +0300

    feat: install & config plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => ({
+  build: {
+    brotliSize: false,
+    manifest: false,
+    minify: mode === 'development' ? false : true,
+    outDir: 'dist',
+    sourcemap: command === 'serve' ? 'inline' : false,
+  },
+
+  plugins: [react(), eslint()],
+}));
