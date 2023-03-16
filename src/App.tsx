aa0c456572bfc2d7450d82441f68d9184593978b
kostili-tec <weh4teyou@gmail.com>
2023-03-16 18:59:18 +0300

    refactor: rewrite App.tsx to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import MainPage from './components/pages/MainPage';
 import ErrorPage from './components/pages/ErrorPage';
 import AboutPage from './components/pages/AboutPage';
@@ -5,18 +6,20 @@ import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route id="Main Page" index element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route id="Main Page" index element={<MainPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </>
+    );
+  }
 }
 
 export default App;
