1ac6c7672bf4497319e6bc86689f8a2b21cdd84b
kostili-tec <weh4teyou@gmail.com>
2023-03-28 19:49:31 +0300

    feat: add text to about page

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from 'react';
 
 const AboutPage: FC = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <h3 style={{ marginTop: '2rem' }}>
+        Welcome to the About page of our website! My name is Danil and I am a web developer with a
+        passion for creating cutting-edge web applications using the latest technologies. One of my
+        favorite frameworks to work with is React, which allows me to build dynamic and responsive
+        user interfaces that enhance the user experience. I am constantly learning and exploring new
+        ideas to improve my skills and stay ahead of the curve in this ever-evolving field.
+      </h3>
+    </div>
+  );
 };
 
 export default AboutPage;
