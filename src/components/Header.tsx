5e716f1dc354f74df39fb0666e62c5f45c2d0dd8
kostili-tec <weh4teyou@gmail.com>
2023-04-02 13:31:41 +0300

    fix: header current page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,24 @@
-import React, { FC, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { FC, useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import classes from '../styles/header.module.scss';
 import { IStateHeader, IHeaderProps } from '../interfaces';
 
 const Header: FC<IHeaderProps> = (props) => {
+  const { headerLinks } = props;
   const [header, setHeader] = useState<IStateHeader>({ title: 'Main Page' });
+  const location = useLocation();
 
-  const changePageTitle = (name: string) => {
-    setHeader({ title: name });
-  };
+  useEffect(() => {
+    const currenPage = headerLinks.find((page) => page.linkTo === location.pathname);
+    if (currenPage) setHeader({ title: currenPage.linkName });
+  }, [location, headerLinks]);
+
   return (
     <header className={classes.header}>
       <h1>{header.title}</h1>
       <nav className={classes.nav}>
-        {props.headerLinks.map((value, index) => (
-          <NavLink
-            key={index}
-            to={value.linkTo}
-            className={classes['header-link']}
-            onClick={() => changePageTitle(value.linkName)}
-          >
+        {headerLinks.map((value, index) => (
+          <NavLink key={index} to={value.linkTo} className={classes['header-link']}>
             {value.linkName}
           </NavLink>
         ))}
